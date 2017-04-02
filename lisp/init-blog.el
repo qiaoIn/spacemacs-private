05f148199f302754751c2d2f6e11132afc8a6fb1
qiaoIn <qiao.liubing@gmail.com>
2017-04-02 18:21:22 +0800

    4th try

--- lisp/init-blog.el
+++ lisp/init-blog.el
@@ -18,6 +18,8 @@
          :sitemap-sort-files anti-chronologically
          :sitemap-style list
          :with-latex t
+         :html-head-extra
+         "<link rel=\"icon\" href=\"favicon.ico\">"   ;; 设置网帐logo \TODO
          :html-preamble
          "<div class=\"header\">
               <a href=\"https://aprilwith.me\">自在随心</a> | <a href=\"http://github.com/qiaoin\">Github</a>
@@ -27,25 +29,30 @@
            "Do not show disqus for Archive and Recent Posts"
            (cond ((string= (car (plist-get info :title)) "Sitemap") "")
                  ((string= (car (plist-get info :title)) "Recent Posts")
-                  "<div id=\"sitmap\"><a href=\"https://aprilwith.me/index.html\">Other posts</a></div>")
+                  "<div id=\"sitemap\"><a href=\"https://aprilwith.me\">Other posts</a></div>")
                  (t
-             "<div id=\"sitemap\"><a href=\"https://aprilwith.me/index.html\">Other posts</a></div>
-              <div id=\"disqus_thread\"></div>
-              <script type=\"text/javascript\">
-              /* * * CONFIGURATION VARIABLES: EDIT BEFORE PASTING INTO YOUR WEBPAGE * * */
-              var disqus_shortname = 'bastibe';
-              /* * * DON'T EDIT BELOW THIS LINE * * */
-              (function() {
-                var dsq = document.createElement('script');
-                dsq.type = 'text/javascript';
-                dsq.async = true;
-                dsq.src = 'http://' + disqus_shortname + '.disqus.com/embed.js';
-                (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
+                  "<div id=\"disqus_thread\"></div>
+                  <script>
+                  /**
+                   *  RECOMMENDED CONFIGURATION VARIABLES: EDIT AND UNCOMMENT THE SECTION BELOW TO INSERT DYNAMIC VALUES FROM YOUR PLATFORM OR CMS.
+                   *  LEARN WHY DEFINING THESE VARIABLES IS IMPORTANT: https://disqus.com/admin/universalcode/#configuration-variables
+                   */
+                  /*
+                  var disqus_config = function () {
+                    this.page.url = PAGE_URL;  // Replace PAGE_URL with your page's canonical URL variable
+                    this.page.identifier = PAGE_IDENTIFIER; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
+                  };
+                  */
+                  (function() { // DON'T EDIT BELOW THIS LINE
+                    var d = document, s = d.createElement('script');
+                    s.src = 'https://aprilwith.disqus.com/embed.js';
+                    s.setAttribute('data-timestamp', +new Date());
+                    (d.head || d.body).appendChild(s);
                   })();
-              </script>
-              <noscript>Please enable JavaScript to view the
-                  <a href=\"http://disqus.com/?ref_noscript\">comments powered by Disqus.</a></noscript>
-              <a href=\"http://disqus.com\" class=\"dsq-brlink\">comments powered by <span class=\"logo-disqus\">Disqus</span></a>")))
+                  </script>
+                  <noscript>
+                    Please enable JavaScript to view the <a href=\"https://disqus.com/?ref_noscript\">comments powered by Disqus.</a>
+                  </noscript>")))
          :author "qiaoin"
          :email "qiao dot liubing at gmail dot com"
          :html-head "<link rel=\"stylesheet\" type=\"text/css\" href=\"css/site.css\"/>"
